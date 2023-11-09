4624f7d1012ce88966fb368a44de269cb04645e3
anshT <ansh.tyagi@acefone.com>
2023-11-09 16:53:48 +0530

    docker image seg fault fix

--- CRUD/api/routes/userRoutes.ts
+++ CRUD/api/routes/userRoutes.ts
@@ -5,7 +5,7 @@ import auth from "../middlewares/auth.ts"
 const userRouter: Router = Router()
 
 userRouter.get('/',auth,UserController.getUsers)
-userRouter.post('/signUp', UserController.signUpUser)
+userRouter.post('/signUp',UserController.signUpUser)
 userRouter.post('/login',UserController.loginUser)
 userRouter.patch('/edit',auth,UserController.editUser)
 userRouter.delete('/remove',auth,UserController.deleteUser)
